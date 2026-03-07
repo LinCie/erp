76494ccbddfc89dbce50e52b588b6059a30796ba
LinCie <aldiofernanda@protonmail.com>
2026-03-08 01:53:23 +0700

    feat: Implement authentication UI and pages, including global providers, assets, and related dependencies.

---
+++ src/app/providers.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Link from "next/link";
+import { authClient } from "@/lib/auth/auth-client";
+import { AuthUIProvider } from "@daveyplate/better-auth-ui";
+import { useRouter } from "next/navigation";
+import type { ReactNode } from "react";
+
+export function Providers({ children }: { children: ReactNode }) {
+  const router = useRouter();
+
+  return (
+    <AuthUIProvider
+      authClient={authClient}
+      navigate={router.push}
+      replace={router.replace}
+      onSessionChange={() => {
+        router.refresh();
+      }}
+      Link={Link}
+    >
+      {children}
+    </AuthUIProvider>
+  );
+}
